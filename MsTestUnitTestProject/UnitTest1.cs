63f3a4a6a03ef255f2b992c146a46704faea40c2
Vagisha Nidhi <vagisha.nda@gmail.com>
2019-11-14 13:52:24 +0530

    Adding Yamls and test projects

---
+++ MsTestUnitTestProject/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MsTestUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(2, 2);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
