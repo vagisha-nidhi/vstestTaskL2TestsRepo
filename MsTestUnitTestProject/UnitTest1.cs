0ca9276968cbf07419e1f2d36d7cb94430cc23d3
Vagisha Nidhi <vanidhi@microsoft.com>
2019-11-20 23:24:30 +0530

    Adding priority to test cases

--- MsTestUnitTestProject/UnitTest1.cs
+++ MsTestUnitTestProject/UnitTest1.cs
@@ -6,30 +6,35 @@ namespace MsTestUnitTestProject
     public class UnitTest1
     {
         [TestMethod]
+        [Priority(0)]
         public void TestMethod1()
         {
             Assert.AreEqual(1, 1);
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod2()
         {
             Assert.AreEqual(2, 2);
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod3()
         {
             Assert.AreEqual(1, 1);
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod4()
         {
             Assert.AreEqual(1, 1);
         }
 
         [TestMethod]
+        [Priority(0)]
         public void TestMethod5()
         {
             Assert.AreEqual(1, 1);
